46d430fd1273f538965c35fc3cbe3a559c18d952
jlbuild <juliabuildbot@gmail.com>
2026-03-06 07:57:09 +0000

    prrte_jll build 4.1.0+0

--- src/prrte_jll.jl
+++ src/prrte_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("prrte")
-JLLWrappers.@generate_main_file("prrte", UUID("eb928a42-fffd-568d-ab9c-3f5d54fc65b9"))
+JLLWrappers.@generate_main_file("prrte", Base.UUID("eb928a42-fffd-568d-ab9c-3f5d54fc65b9"))
 end  # module prrte_jll
